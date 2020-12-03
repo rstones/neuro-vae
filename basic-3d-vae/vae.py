c87d4331369008e3239e1f900872cb12c6edb5a0
Richard Stones <richardstones10@gmail.com>
2020-12-03 14:59:44 +0000

    Sketched out the VAE lightning module class

---
+++ basic-3d-vae/vae.py
@@ -0,0 +1,38 @@
+import torch
+import torch.optim as optim
+import pytorch_lightning as pl
+
+class VAE(pl.LightningModule):
+
+    def __init__(self):
+        super().__init__()
+        
+        # define the ConvNet_Down, AE and ConvNet_Up bits here
+
+    def forward(self, x):
+        # link the bits together here
+        pass
+
+    def training_step(self, batch, batch_idx):
+        noisy_data = batch['data']
+        clean_data = batch['seg'] # seg is batchgenerators default for labels
+        pred = self(noisy_data)
+        loss = 0 # compare clean_data and pred here
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        noisy_data = batch['data']
+        clean_data = batch['seg'] # seg is batchgenerators default for labels
+        pred = self(noisy_data)
+        loss = 0 # compare clean_data and pred here
+        self.log('val_loss', loss)
+        return loss
+
+
+    def test_step(self, batch, batch_idx):
+        pass
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1.e-3)
+        return optimizer
